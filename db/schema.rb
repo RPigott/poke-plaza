8d8fbd6efc9ecb4b7d5cd31a591781f90122ff8d
Joseph Chen <j.joseph.chen@berkeley.edu>
2017-01-18 00:50:04 +0000

    controllers and models for Pokemons and Users

---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170118002036) do
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string   "species"
+    t.string   "form"
+    t.string   "nickname"
+    t.string   "gender"
+    t.boolean  "shiny"
+    t.string   "nature"
+    t.string   "ability"
+    t.integer  "HPIV"
+    t.integer  "AtkIV"
+    t.integer  "DefIV"
+    t.integer  "SpAIV"
+    t.integer  "SpDIV"
+    t.integer  "SpeIV"
+    t.string   "hiddenpower"
+    t.string   "move1"
+    t.string   "move3"
+    t.string   "move2"
+    t.string   "move4"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "ign"
+    t.string   "friendcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
