e78649f11724371d52e9b4a01c595698489ca5fe
Ronan Pigott <rpigott@berkeley.edu>
2017-01-20 12:25:53 -0800

    added form sprites

--- app/helpers/pokemons_helper.rb
+++ app/helpers/pokemons_helper.rb
@@ -1,5 +1,2 @@
 module PokemonsHelper
-	def sprite_name
-		return self.species.name.delete("'").sub(/[^\w0-9]/, "-").downcase
-	end
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,4 +2,12 @@ class Pokemon < ApplicationRecord
 	belongs_to :species
 	belongs_to :user
 	has_many :moves
+
+	def sprite_name
+		return self.species.name.delete("'").sub(/[^\w0-9]/, "-").downcase
+	end
+
+	def form_name
+		return self.species.form.sub(/[^\w0-9]/, "-").downcase
+	end
 end
