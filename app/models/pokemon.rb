d91822d19f1594a8927338e30e8f788f5a1475b9
Ronan Pigott <rpigott@berkeley.edu>
2017-01-20 12:37:57 -0800

    fixed some form names

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,10 +4,10 @@ class Pokemon < ApplicationRecord
 	has_many :moves
 
 	def sprite_name
-		return self.species.name.delete("'").sub(/[^\w0-9]/, "-").downcase
+		return self.species.name.delete("'").sub(/[^A-Za-z0-9]/, "-").downcase
 	end
 
 	def form_name
-		return self.species.form.sub(/[^\w0-9]/, "-").downcase
+		return self.species.form.split.first.sub(/[^A-Za-z0-9]/, "-").downcase
 	end
 end
