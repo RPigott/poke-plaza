2bf262bac69b12193fab78f30b189260acb0b520
Ronan Pigott <rpigott@berkeley.edu>
2017-01-19 21:45:42 -0800

    Allowed PkSpr to process DOM for pkmn menu icons.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( pokesprite.js )
-Rails.application.config.assets.precompile += %w( pokesprite.scss )
+Rails.application.config.assets.precompile += %w( pokesprite.js pokesprite.scss )
