7ae8f1ec59e7940b6ca761d94cd981bcf4b5010a
Ronan Pigott <rpigott@berkeley.edu>
2017-01-20 18:51:49 -0800

    Fixed optional moveslots

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon < ApplicationRecord
 	belongs_to :species
 	belongs_to :user
 	belongs_to :move1, class_name: "Move"
-	belongs_to :move2, class_name: "Move"
-	belongs_to :move3, class_name: "Move"
-	belongs_to :move4, class_name: "Move"
+	belongs_to :move2, class_name: "Move", optional: true
+	belongs_to :move3, class_name: "Move", optional: true
+	belongs_to :move4, class_name: "Move", optional: true
 
 	def sprite_name
 		return self.species.name.delete("'").sub(/[^A-Za-z0-9]/, "-").downcase
