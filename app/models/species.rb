7b330c86ee636eb5a4e46643f541da8572b2979a
Ronan Pigott <rpigott@berkeley.edu>
2017-01-20 16:12:55 -0800

    cleaned up learnset associations

--- app/models/species.rb
+++ app/models/species.rb
@@ -6,4 +6,8 @@ class Species < ApplicationRecord
 	has_and_belongs_to_many :egg_moves,       join_table: :egg_moves,       class_name: "Move"
 	has_and_belongs_to_many :tutor_moves,     join_table: :tutor_moves,     class_name: "Move"
 	has_and_belongs_to_many :evolution_moves, join_table: :evolution_moves, class_name: "Move"
+
+	def moves
+		return self.levelup_moves + self.tm_moves + self.egg_moves + self.tutor_moves + self.evolution_moves
+	end
 end
