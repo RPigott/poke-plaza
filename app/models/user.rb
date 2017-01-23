cb603de433b77b2f9cfde51de0ca20bbc1cb67f1
Joseph Chen <j.joseph.chen@berkeley.edu>
2017-01-23 09:22:37 +0000

    Some css awesome

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :pokemons
+	
+	# before_destroy :destroy_pokemons
 
 	def email_required?
 		false
