7447afc428bc8e5758c70c7115560758bbbd6889
Joseph Chen <j.joseph.chen@berkeley.edu>
2017-01-25 23:02:43 +0000

    Finished styling user log in / sign up. Users can now sign up and log in correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
 	    user_params.permit(:username, :email)
 	  end
   end
+  
+  def after_sign_in_path_for(user)
+     user_path(user)
+  end
+  
+  def after_sign_out_path_for(user)
+      root_path
+  end
 end
